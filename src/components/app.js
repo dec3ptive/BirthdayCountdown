daa05b838f40f5b6c25d69406ee10a0169e04603
Quinn Ormond <quinnormond2000@gmail.com>
2018-07-16 09:34:26 -0600

    added first set of skews

---
+++ src/components/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div classname = "grid">
+          <div>
+            h1
+          </div>
+          <div>
+            h1
+          </div>
+          <div>
+            h1
+          </div>
+          <div className = "grid_skew-light-one">
+            
+          </div>
+          <div className = "grid_skew-light-two">
+          
+          </div>
+          <div className = "grid_skew-light-three-box">
+          
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
